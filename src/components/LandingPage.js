ad452e4e84fdeae81838b062311a28379b38a096
Priti Gupta <priticse2633@gmail.com>
2025-11-26 18:08:16 +0530

    upadte landingpage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { updateUserName, updateMonthlyBudget, updateCategoricalBudget } from "../redux/userSlice";
+import {
+  updateUserName,
+  updateMonthlyBudget,
+  updateCategoricalBudget,
+} from "../redux/userSlice";
 import { resetAllExpense } from "../redux/expenseSlice";
 import store from "../redux/store";
 
@@ -37,7 +41,14 @@ const LandingPage = () => {
 
     dispatch(updateUserName(name));
     dispatch(updateMonthlyBudget(total));
-    dispatch(updateCategoricalBudget({ food: foodBudget, travel: travelBudget, entertainment: entertainmentBudget, others }));
+    dispatch(
+      updateCategoricalBudget({
+        food: foodBudget,
+        travel: travelBudget,
+        entertainment: entertainmentBudget,
+        others,
+      })
+    );
     dispatch(resetAllExpense());
 
     navigate("/tracker");
@@ -46,15 +57,55 @@ const LandingPage = () => {
   return (
     <div className="landing-page">
       <h1>Expense Tracker</h1>
-      <form name="landing-page-form" onSubmit={handleSubmit}>
-        <input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-        <input placeholder="Total Budget" type="number" value={totalBudget} onChange={(e) => setTotalBudget(e.target.value)} />
-        <input placeholder="Food" type="number" value={food} onChange={(e) => setFood(e.target.value)} />
-        <input placeholder="Travel" type="number" value={travel} onChange={(e) => setTravel(e.target.value)} />
-        <input placeholder="Entertainment" type="number" value={entertainment} onChange={(e) => setEntertainment(e.target.value)} />
-        <button id="new-update" type="submit">New/Update tracker</button>
+
+      <form id="landing-page-form" onSubmit={handleSubmit}>
+        <input
+          id="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          id="total-budget"
+          placeholder="Total Budget"
+          type="number"
+          value={totalBudget}
+          onChange={(e) => setTotalBudget(e.target.value)}
+        />
+
+        <input
+          id="food-budget"
+          placeholder="Food"
+          type="number"
+          value={food}
+          onChange={(e) => setFood(e.target.value)}
+        />
+
+        <input
+          id="travel-budget"
+          placeholder="Travel"
+          type="number"
+          value={travel}
+          onChange={(e) => setTravel(e.target.value)}
+        />
+
+        <input
+          id="entertainment-budget"
+          placeholder="Entertainment"
+          type="number"
+          value={entertainment}
+          onChange={(e) => setEntertainment(e.target.value)}
+        />
+
+        <button id="new-update" type="submit">
+          New/Update tracker
+        </button>
       </form>
-      <button id="clear" onClick={() => window.location.reload()}>Start new tracker</button>
+
+      <button id="clear" onClick={() => window.location.reload()}>
+        Start new tracker
+      </button>
     </div>
   );
 };
