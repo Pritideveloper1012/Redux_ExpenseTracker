b8f4274efd94c614adb06a210be476ea49e5afcc
Priti Gupta <priticse2633@gmail.com>
2025-11-26 20:04:14 +0530

    upadte landingpage

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
-import store from "./redux/store"; // make sure this is imported
-import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-// For Cypress testing
+// Expose store globally for Cypress
 if (window.Cypress) {
   window.store = store;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
 
-reportWebVitals();
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
